6de3ab22b1d008e37746734f521060ef37014b6d
Eugene Borzov <borzovplus@yandex.ru>
2022-01-06 21:39:38 +0300

    f

--- sqlite3_libsqlite3.go
+++ sqlite3_libsqlite3.go
@@ -9,11 +9,5 @@ package sqlite3
 
 /*
 #cgo CFLAGS: -DUSE_LIBSQLITE3
-#cgo linux LDFLAGS: -lsqlite3
-#cgo darwin LDFLAGS: -L/usr/local/opt/sqlite/lib -lsqlite3
-#cgo darwin CFLAGS: -I/usr/local/opt/sqlite/include
-#cgo openbsd LDFLAGS: -lsqlite3
-#cgo solaris LDFLAGS: -lsqlite3
-#cgo windows LDFLAGS: -lsqlite3
 */
 import "C"
